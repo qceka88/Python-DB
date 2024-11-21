c7bf034ab983aa220200eb022428a893df7aa271
Yanko Rashkov <yanko.rashkov@gmail.com>
2024-11-21 14:03:44 +0200

    Added files ot 03-Data-Types-Lab

---
+++ PostgreSQL/03-DataTypes-Lab/01_create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees
+(
+    id            SERIAL PRIMARY KEY NOT NULL,
+    first_name    VARCHAR(30),
+    last_name     VARCHAR(50),
+    hiring_date   DATE DEFAULT '2023-01-01',
+    salary        NUMERIC(10, 2),
+    devices_number INTEGER
+);
+
+CREATE TABLE departments
+(
+    id          SERIAL PRIMARY KEY UNIQUE,
+    name        VARCHAR(50),
+    code        CHARACTER(3),
+    description TEXT
+);
+
+CREATE TABLE issues
+(
+    id          SERIAL PRIMARY KEY UNIQUE,
+    description VARCHAR(150),
+    date        DATE,
+    start       TIMESTAMP
+);
---
+++ PostgreSQL/03-DataTypes-Lab/02inser_data_in_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees(first_name, last_name, hiring_date, salary, devices_number)
+VALUES
+    ('Nikodim', 'Stamatov', '2012-02-02', 1200.35, 123),
+    ('Brane', 'Stamatov', '2018-02-02', 1500.35, 133),
+    ('Stoyo', 'Stamatov', '2015-02-02', 1500.35, 143)
+;
