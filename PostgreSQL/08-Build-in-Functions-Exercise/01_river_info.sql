1e231f9048706a52d9735944f5d4eedef2fa12c2
Yanko Rashkov <yanko.rashkov@gmail.com>
2024-11-28 13:22:51 +0200

    Added files to 08-Build-in-Functions-Exercise

---
+++ PostgreSQL/08-Build-in-Functions-Exercise/01_river_info.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW view_river_info AS
+SELECT concat('The river', ' ', river_name, ' ', 'flows into the', ' ', outflow, ' ', 'and is', ' ', "length", ' ',
+              'kilometers long.') as "River Information"
+FROM rivers
+ORDER BY river_name;
+
